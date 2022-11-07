2378a4130744dfa1b43969e450abfab7d700f412
jinyihuang-mock <854410806@qq.com>
2022-11-08 04:36:33 +0800

    3.3 首页布局 和路由

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    yellow 第三次 写
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'the-footer',
+});
+</script>
