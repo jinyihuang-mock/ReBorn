3f83616605bf7a86af24c07a91982dc71d1db508
85441 <854410806@qq.com>
2022-11-09 01:45:54 +0800

    5.1 集成axios

---
+++ web/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <a-layout>
+    <the-header></the-header>
+    <router-view/>
+    <the-footer></the-footer>
+  </a-layout>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import TheFooter from "@/components/the-footer.vue";
+import TheHeader from "@/components/the-header.vue";
+
+
+export default defineComponent({
+  name: 'app',
+  components: {TheFooter, TheHeader},
+  props: {
+    TheFooter,
+    TheHeader,
+  },
+});
+</script>
