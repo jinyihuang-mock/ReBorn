3c5182c2c1555d756c246e4424669f484ffe652f
85441 <854410806@qq.com>
2023-01-03 22:53:19 +0800

    8-11  增加文档读取功能

--- src/main/java/com/jiawa/wiki/service/DocService.java
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -109,4 +109,8 @@ public class DocService {
         criteria.andIdIn(ids);
         docMapper.deleteByExample(docExample);
     }
+    public String findContent(Long id){
+        Content content = contentMapper.selectByPrimaryKey(id);
+        return content.getContent();
+    }
 }
